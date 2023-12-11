0a381399df59d7ff3d6c57efdac42b939a633cab
Isabel Robleda <isabel.robleda@gmail.com>
2023-12-11 14:51:42 +0100

    addition of allCV page and position field

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,59 @@
-import { useState } from 'react'
-import SignUpPage from './pages/SignupPage';
+import { useState } from "react";
+import SignUpPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import IsPrivate from "./components/IsPrivate";
 import IsAnon from "./components/IsAnon";
-import CurriculumPage from './pages/CurriculumPage';
-import HomePage from './pages/HomePage';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-
+import CurriculumPage from "./pages/CurriculumPage";
+import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import AllCurriculumsPage from "./pages/AllCurriculumsPage";
+import { DropdownProvider } from "./context/DropdownContext";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>      
-        <Route path="/" element={<HomePage />} />
-        <Route path="/curriculum" element={ <IsPrivate><CurriculumPage /></IsPrivate> } />
-        <Route path="/login" element={<IsAnon> <LoginPage /> </IsAnon>} />
-        <Route path="/signup" element={<IsAnon> <SignUpPage /> </IsAnon>} />
-      </Routes>
-      
+      <DropdownProvider>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/curriculum"
+            element={ 
+              <IsPrivate>
+                <CurriculumPage />
+              </IsPrivate>
+            }
+          />
+          <Route
+            path="/my-curriculums"
+            element={
+              <IsPrivate>
+                <AllCurriculumsPage />
+              </IsPrivate>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <IsAnon>
+                {" "}
+                <LoginPage />{" "}
+              </IsAnon>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <IsAnon>
+                {" "}
+                <SignUpPage />{" "}
+              </IsAnon>
+            }
+          />
+        </Routes>
+      </DropdownProvider>
     </div>
   );
 }
