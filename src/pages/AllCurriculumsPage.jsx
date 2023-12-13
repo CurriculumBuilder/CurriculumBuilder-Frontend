9d857ad6a97fba4d4ee7617950cf7185aed7f795
Isabel Robleda <isabel.robleda@gmail.com>
2023-12-13 15:40:47 +0100

    create contact form and css optimization

--- src/pages/AllCurriculumsPage.jsx
+++ src/pages/AllCurriculumsPage.jsx
@@ -7,6 +7,7 @@ import "../styles/AllCurriculums.css"
 Modal.setAppElement("#root");
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import janeDoeCv from "../assets/janeDoeCv.png"
 
 
 function AllCurriculumsPage() {
@@ -80,27 +81,28 @@ function AllCurriculumsPage() {
     <>
       <h1 className='font-bold text-slate-800 text-4xl p-5'>My Curriculums</h1>
 
-      <section className="bg-base-100 gap-4 m-2 md:grid md:grid-cols-2 md:gap-4">
-        {curriculums.map((curriculum) => (
-          <div key={curriculum._id} className="flex min-w-0 gap-x-4 shadow-lg m-2 w-full grid-cols-2 p-3 mr-2">
-            <div className="min-w-0 flex-auto">
-              <Link to={`./update-curriculum/${curriculum._id}`}>
-                <h3 className="text-sm font-semibold leading-6 text-gray-900 ml-2">{curriculum.personalData.name}</h3>
-                <img src="https://t3.ftcdn.net/jpg/02/48/42/64/240_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg" alt="" />
-                <p className='mt-1 truncate text-xs leading-5 text-gray-500 ml-2'>{curriculum.personalData.position}</p>
-              </Link>
+      <section className="bg-base-100 gap-4 m-2 md:grid md:grid-cols-2 md:gap-4 justify-center items-center">
+  {curriculums.map((curriculum) => (
+    <div key={curriculum._id} className="flex items-center min-w-0 gap-x-4 shadow-lg m-2 w-full grid-cols-2 p-3 mr-2">
+      <div className="min-w-0 flex-auto text-center">
+        <Link to={`./update-curriculum/${curriculum._id}`}>
+          
+          <h3 className="text-lg mt-2 font-semibold leading-6 text-gray-900">{curriculum.personalData.name}</h3>
+          <p className='mt-1 mb-2 p-3 text-md leading-5 text-gray-600'>{curriculum.personalData.position}</p>
+          <img className="mx-auto w-72" src={janeDoeCv} alt="" />
+          
+        </Link>
 
-              <button
-                className="inline-flex font-semibold items-center justify-center rounded-md p-2.5 text-red-800 shadow-md hover:bg-red-800 hover:text-white m-2"
-                onClick={() => openModal(curriculum._id)}
-              >
-                Delete
-              </button>
-              
-            </div>
-          </div>
-        ))}
-      </section>
+        <button
+          className="inline-flex font-semibold items-center justify-center rounded-md p-2.5 text-red-800 shadow-md hover:bg-red-800 hover:text-white mt-5 mb-3"
+          onClick={() => openModal(curriculum._id)}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  ))}
+</section>
       
       <Modal
         isOpen={modalIsOpen}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,108 +1,134 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
-import { HashLink } from "react-router-hash-link";
 import "../styles/HomePage.css";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
-import  exampleCV from "../assets/exampleCV.png"
+import janeDoeCv from "../assets/janeDoeCv.png";
 
 function HomePage() {
   const { isLoggedIn } = useContext(AuthContext);
 
   return (
     <>
-    <div className="content-home">
-      <section className="main-section-home">
-        <div className="text-main-section">
-          <h1 className="titles-home">
-            Studies show that recruiters spend an average of 6 seconds reviewing
-            a resume
-          </h1>
-          <p className="p-main-section">
-            Make those seconds count! Create a compelling CV to ensure that your
-            skills and achievements grab immediate attention. Your future career
-            success starts with those crucial 6 seconds – maximize your impact
-            with a standout resume.
-          </p>
-
-          {isLoggedIn && (
-            <Link to={"/curriculum"}>
-              <button className="btnHome">Create CV </button>
-            </Link>
-          )}
-
-        {!isLoggedIn && (
-            <Link to={"/signup"}>
-              <button className="btnHome">Create Account </button>
-            </Link>
-          )}
-        </div>
-        <img
-            src={exampleCV}
-            alt="cv-image"
-            className="imgExample"
-          />
-      </section>
-
-      <br />
-
-      <HashLink to="/#about-us">
-        <section id="about-us">
-          <div>
-            <h1 className="titles-home">About Us</h1>
+    <div className="home">
+      <div className="content-home">
+        <section className="main-section-home" id="home">
+          <div className="text-main-section">
+            <h1 className="titles-home">
+              Studies show that recruiters spend an average of 6 seconds
+              reviewing a resume
+            </h1>
             <p className="p-main-section">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-              ullamcorper magna malesuada metus hendrerit gravida. In congue
-              tincidunt augue, nec bibendum nisl vehicula lacinia. Duis mollis,
-              est sit amet gravida ultricies, dui nisl pellentesque justo, vitae
-              venenatis ex enim at est. Vivamus fringilla, nunc non rutrum
-              viverra, risus nisl pellentesque risus, in blandit nulla urna
-              vitae ligula.
+              Make those seconds count! Create a compelling CV to ensure that
+              your skills and achievements grab immediate attention. Your future
+              career success starts with those crucial 6 seconds – maximize your
+              impact with a standout resume.
             </p>
+
+            {isLoggedIn && (
+              <Link to={"/curriculum"}>
+                <button className="btnHome">Create CV </button>
+              </Link>
+            )}
+
+            {!isLoggedIn && (
+              <Link to={"/signup"}>
+                <button className="btnHome">Create Account </button>
+              </Link>
+            )}
           </div>
+          <img src={janeDoeCv} alt="cv-image" className="imgExample" />
         </section>
-      </HashLink>
 
-      <br />
+        <br />
 
-      <HashLink to="/#contact">
-        <section id="contact">
-          <div>
-            <h1 className="titles-home">Contact</h1>
-            <p className="p-main-section">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-              ullamcorper magna malesuada metus hendrerit gravida. In congue
-              tincidunt augue, nec bibendum nisl vehicula lacinia. Duis mollis,
-              est sit amet gravida ultricies, dui nisl pellentesque justo, vitae
-              venenatis ex enim at est. Vivamus fringilla, nunc non rutrum
-              viverra, risus nisl pellentesque risus, in blandit nulla urna
-              vitae ligula.
-            </p>
-          </div>
-        </section>
-      </HashLink>
-
-      <br />
-
-      <HashLink to="/#faq">
-        <section id="faq">
-          <div>
-            <h1 className="titles-home">FAQ</h1>
-            <p className="p-main-section">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-              ullamcorper magna malesuada metus hendrerit gravida. In congue
-              tincidunt augue, nec bibendum nisl vehicula lacinia. Duis mollis,
-              est sit amet gravida ultricies, dui nisl pellentesque justo, vitae
-              venenatis ex enim at est. Vivamus fringilla, nunc non rutrum
-              viverra, risus nisl pellentesque risus, in blandit nulla urna
-              vitae ligula.
-            </p>
-          </div>
-        </section>
-      </HashLink>
+          <section id="about-us">
+            <div>
+              <h1 className="titles-home">About Us</h1>
+              <p className="p-main-section">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                ullamcorper magna malesuada metus hendrerit gravida. In congue
+                tincidunt augue, nec bibendum nisl vehicula lacinia. Duis
+                mollis, est sit amet gravida ultricies, dui nisl pellentesque
+                justo, vitae venenatis ex enim at est. Vivamus fringilla, nunc
+                non rutrum viverra, risus nisl pellentesque risus, in blandit
+                nulla urna vitae ligula.
+              </p>
+            </div>
+          </section>
+       
+        <br />
       </div>
+      
+          <section id="contact" className="bg-white dark:bg-slate-800">
+            <div className="py-8 lg:py-16 px-8 mx-auto max-w-screen-md">
+              <h2 className="mb-4 text-4xl tracking-tight font-semibold  text-gray-900 dark:text-white">
+                Contact Us
+              </h2>
+              <p className="mb-8 lg:mb-16 font-medium text-gray-500 dark:text-gray-400 sm:text-xl">
+                Got a technical issue? Want to send feedback about a feature?
+                Wanna say "hi"? Let us know.
+              </p>
+              <form action="#" className="space-y-8">
+                <div>
+                  <label
+                    for="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    placeholder="example@example.com"
+                    required
+                  ></input>
+                </div>
+                <div>
+                  <label
+                    for="subject"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Subject
+                  </label>
+                  <input
+                    type="text"
+                    id="subject"
+                    className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                    placeholder="Let us know how we can help you"
+                    required
+                  ></input>
+                </div>
+                <div className="sm:col-span-2">
+                  <label
+                    for="message"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                  >
+                    Your message
+                  </label>
+                  <textarea
+                    id="message"
+                    rows="6"
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="Leave a comment..."
+                  ></textarea>
+                </div>
+                <button
+                  type="submit"
+                  className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Send message
+                </button>
+              </form>
+            </div>
+            <br />
+            <br />
+          </section>
+        
+        </div>
       <Footer />
-      </>
+    </>
   );
 }
 
