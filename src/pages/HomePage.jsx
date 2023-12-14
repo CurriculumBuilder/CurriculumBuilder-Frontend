dbf5ae9188a02628c854472a785f44b618d84e0a
Isabel Robleda <isabel.robleda@gmail.com>
2023-12-14 09:31:39 +0100

    addition of light and dark theme for contact form

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -60,9 +60,9 @@ function HomePage() {
         <br />
       </div>
       
-          <section id="contact" className="bg-white dark:bg-slate-800">
+          <section id="contact" className="bg-slate-800">
             <div className="py-8 lg:py-16 px-8 mx-auto max-w-screen-md">
-              <h2 className="mb-4 text-4xl tracking-tight font-semibold  text-gray-900 dark:text-white">
+              <h2 className="mb-4 text-4xl tracking-tight font-semibold text-white">
                 Contact Us
               </h2>
               <p className="mb-8 lg:mb-16 font-medium text-gray-500 dark:text-gray-400 sm:text-xl">
@@ -110,7 +110,7 @@ function HomePage() {
                   <textarea
                     id="message"
                     rows="6"
-                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    className="block p-2.5 w-full text-sm rounded-lg shadow-sm border focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500"
                     placeholder="Leave a comment..."
                   ></textarea>
                 </div>
